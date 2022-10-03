4b4255b61e9d20508d95667e8d29c0c40d0fcc76
David <lovedavidren@outlook.com>
2022-10-03 16:39:12 +0800

    add G6

--- test_sytoscape/test/code.js
+++ test_sytoscape/test/code.js
@@ -11,50 +11,52 @@ var cy = cytoscape({
 
   style: cytoscape.stylesheet()
     .selector('node').style({
-        'content': 'data(id)',
-        'width': 20,
-        'height': 20,
-        'background-color': '#666'
-      })
+      'content': 'data(id)',
+      'width': 20,
+      'height': 20,
+      'background-color': '#666'
+    })
     .selector('edge').style({
-        'curve-style': 'bezier',
-        'target-arrow-shape': 'triangle',
-        'width': 3,
-        'line-color': '#666',
-        'target-arrow-color': '#666'
-      })
+      'curve-style': 'bezier',
+      'target-arrow-shape': 'triangle',
+      'width': 3,
+      'line-color': '#666',
+      'target-arrow-color': '#666'
+    })
     .selector('.highlighted').style({
-        'background-color': '#61bffc',
-        'line-color': '#61bffc',
-        'target-arrow-color': '#61bffc',
-        'transition-property': 'background-color, line-color, target-arrow-color',
-        'transition-duration': '0.5s'
-      }),
+      'background-color': '#61bffc',
+      'line-color': '#61bffc',
+      'target-arrow-color': '#61bffc',
+      'transition-property': 'background-color, line-color, target-arrow-color',
+      'transition-duration': '0.5s'
+    }),
 
   elements: {
     nodes: [
-      { data: { 
-          id: 'a', 
-          commandName: 'kafkasource', 
-          jarUrl: null, "rawArg": "{\"groupId\":\"springboot-consumer02\",\"topic\":\"word-count\",\"cluster\":\"192.168.0.104:9092\",\"consumerOffsets\":\"LATEST\",\"encodingConfig\":{\"encodingType\":\"JSON_OBJECT\"}}" 
-        } 
+      {
+        data: {
+          id: 'a',
+          commandName: 'kafkasource',
+          jarUrl: null, "rawArg": "{\"groupId\":\"springboot-consumer02\",\"topic\":\"word-count\",\"cluster\":\"192.168.0.104:9092\",\"consumerOffsets\":\"LATEST\",\"encodingConfig\":{\"encodingType\":\"JSON_OBJECT\"}}"
+        }
       },
-      { data: { 
+      {
+        data: {
           id: 'b',
           commandName: "kafkasink",
           rawArg: "{\"topic\":\"test_topic\",\"cluster\":\"192.168.0.104:9092\",\"encodingConfig\":{\"encodingType\":\"JSON_OBJECT\"}}"
-         } 
+        }
       }
     ],
 
     edges: [
       { data: { id: 'ab', edgeName: "1_2", source: 'a', target: 'b' } }
     ]
-      },
+  },
 });
 
 // 监听node 点击事件
-cy.on('tap', function(evt){
+cy.on('tap', function (evt) {
   var eventTarget = evt.target;
 
   if (eventTarget.hasClass("highlighted")) {
@@ -70,10 +72,10 @@ $("#create-node").hide();
 $("#create-edge").hide();
 
 
-$('#add-node').click(()=> {$("#create-node").show();$("#create-edge").hide();})
-$('#add-edge').click(()=> {$("#create-edge").show();$("#create-node").hide();})
+$('#add-node').click(() => { $("#create-node").show(); $("#create-edge").hide(); })
+$('#add-edge').click(() => { $("#create-edge").show(); $("#create-node").hide(); })
 
-$('#delete').click(()=>{
+$('#delete').click(() => {
   cy.remove(cy.elements(".highlighted"))
   $("#create-node").hide();
   $("#create-edge").hide();
@@ -81,7 +83,7 @@ $('#delete').click(()=>{
 
 
 
-$('#create-node-yes').click(()=>{
+$('#create-node-yes').click(() => {
   let ele = { // 一个节点
     group: 'nodes', // 'nodes' for a node, 'edges' for an edge
     data: { // 元素数据
@@ -91,8 +93,8 @@ $('#create-node-yes').click(()=>{
       jarUrl: $('#jar-url').val(),
       rawArg: $('#raw-arg').val()
     },
-    position: {x: 100, y: 100}, // 节点位置
-    renderedPosition: {x: 200, y: 200}, // 节点呈现位置,优先级高于position
+    position: { x: 100, y: 100 }, // 节点位置
+    renderedPosition: { x: 200, y: 200 }, // 节点呈现位置,优先级高于position
     selected: false, // 节点被选择
     selectable: true, // 节点可以被选择
     locked: false, // 节点是否被锁定,锁定后,位置不可变
@@ -103,14 +105,14 @@ $('#create-node-yes').click(()=>{
   $("#create-node").hide();
 })
 
-$('#create-edge-yes').click(()=>{
-  let edge =  { // edge e1
+$('#create-edge-yes').click(() => {
+  let edge = { // edge e1
     group: 'edges', /* 'nodes' for a node, 'edges' for an edge,指定了'source'和'target'属性,可省略此属性. */
     data: {
       // id: 'e1',
       name: $('#edge-name').val(),
       /* 因为指定了'source'和'target',所以推断为边缘. `eles.move()`可以有效地更改'source'和'target'的内容. */
-      source: $('#edge-from').val(), /* 起点 */ 
+      source: $('#edge-from').val(), /* 起点 */
       target: $('#edge-to').val(),  /* 终点 */
     },
     selected: false, // 关系被选择
@@ -121,11 +123,11 @@ $('#create-edge-yes').click(()=>{
 })
 
 
-$('#create-node-no').click(()=>{$("#create-node").hide();})
-$('#create-edge-no').click(()=>{$("#create-edge").hide();})
+$('#create-node-no').click(() => { $("#create-node").hide(); })
+$('#create-edge-no').click(() => { $("#create-edge").hide(); })
 
 
-$('#show-fleak-query').click(()=>{
+$('#show-fleak-query').click(() => {
   $("#create-node").hide();
   $("#create-edge").hide();
 
@@ -138,7 +140,7 @@ $('#show-fleak-query').click(()=>{
   var eles = cy.elements()
 
 
-  eles.forEach( each=> {
+  eles.forEach(each => {
     console.log(each.data('id'))
 
     if (each.isNode()) {
